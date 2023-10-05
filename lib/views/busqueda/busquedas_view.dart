d171fba0f9d311b1eeb809adcbe9f058796f602c
Jesús Alejandro Pérez Ramírez <dual.jesus.perez@gmail.com>
2023-10-04 23:23:54 -0600

    Cambios

--- lib/views/busqueda/busquedas_view.dart
+++ lib/views/busqueda/busquedas_view.dart
@@ -24,16 +24,24 @@ class BusquedasPage extends StatefulWidget {
 }
 
 class _BusquedasPageState extends State<BusquedasPage> {
-final _formKey = GlobalKey<FormState>();
-BCUser? user;
-BCColaborador? colaborador;
+  final _formKey = GlobalKey<FormState>();
+  BCUser? user;
+  BCColaborador? colaborador;
 
-@override
-Widget build(BuildContext context) {
-  final userInitials =
-        '${(user?.names ?? '') == '' ? '' : (user?.names ?? '').substring(0, 1)}${(user?.lastNames ?? '') == '' ? '' : (user?.lastNames ?? '').substring(0, 1)}';
-  return Scaffold(
-    appBar: BconnectAppBar(
+  List<String> cedisList = ['Cedis 1', 'Cedis 2', 'Cedis 3'];
+  String? selectedCedis;
+
+  List<String> periodList = ['Periodo 1', 'Periodo 2', 'Periodo 3'];
+  String? selectedPeriod;
+
+  final TextEditingController _searchController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    final userInitials =
+          '${(user?.names ?? '') == '' ? '' : (user?.names ?? '').substring(0, 1)}${(user?.lastNames ?? '') == '' ? '' : (user?.lastNames ?? '').substring(0, 1)}';
+    return Scaffold(
+      appBar: BconnectAppBar(
         onPressed: () => {
           Navigator.push(
               context,
@@ -43,7 +51,94 @@ Widget build(BuildContext context) {
         },
         userInitials: userInitials,
       ),
-    bottomNavigationBar: const NavigationBarComponenet(0), // asume que esto es lo único que necesitas
-  );
-}
+      bottomNavigationBar: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Container(
+              height: 50.0, // Altura ajustada del botón
+              width: double.infinity,
+              child: ElevatedButton(
+                onPressed: () {
+                  // Inserta la lógica para descargar el reporte
+                },
+                style: ButtonStyle(
+                  backgroundColor: MaterialStateProperty.all(Colors.orange),
+                ),
+                child: Text(
+                  'Descargar Reporte',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 18.0, // Tamaño de fuente ajustado
+                  ),
+                ),
+              ),
+            ),
+          ),
+          NavigationBarComponenet(0), // Configura tu NavigationBar
+        ],
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(10.0),
+        child: Form(
+          key: _formKey,
+          child: Column(
+            children: [
+              Row(
+                children: [
+                  Expanded(
+                    child: DropdownButtonFormField(
+                      value: selectedCedis,
+                      decoration: InputDecoration(labelText: 'Cedis'),
+                      items: cedisList.map((cedis) {
+                        return DropdownMenuItem(
+                          value: cedis,
+                          child: Text(cedis),
+                        );
+                      }).toList(),
+                      onChanged: (value) {
+                        setState(() {
+                          selectedCedis = value as String?;
+                        });
+                      },
+                    ),
+                  ),
+                  SizedBox(width: 10),
+                  Expanded(
+                    child: DropdownButtonFormField(
+                      value: selectedPeriod,
+                      decoration: InputDecoration(labelText: 'Periodo'),
+                      items: periodList.map((period) {
+                        return DropdownMenuItem(
+                          value: period,
+                          child: Text(period),
+                        );
+                      }).toList(),
+                      onChanged: (value) {
+                        setState(() {
+                          selectedPeriod = value as String?;
+                        });
+                      },
+                    ),
+                  ),
+                ],
+              ),
+              SizedBox(height: 10),
+              TextFormField(
+                controller: _searchController,
+                decoration: InputDecoration(
+                  labelText: 'Buscar clientes',
+                  prefixIcon: Icon(Icons.search),
+                  border: OutlineInputBorder(),
+                ),
+                // Implementa la lógica de búsqueda
+              ),
+              // ... Añade más widgets si los necesitas
+            ],
+          ),
+        ),
+      ),
+    );
+  }
 }
