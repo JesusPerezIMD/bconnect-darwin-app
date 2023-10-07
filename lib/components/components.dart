82d41c094bf615f83a0e8824d2afc48114a4dfb8
Jesús Alejandro Pérez Ramírez <dual.jesus.perez@gmail.com>
2023-10-06 20:42:25 -0600

    cambios

--- lib/components/components.dart
+++ lib/components/components.dart
@@ -2,3 +2,4 @@ export 'bconnect_appbar.dart';
 export 'app_colors.dart';
 export 'dropdown_component.dart';
 export 'darwin_detail_component.dart';
+export 'custom_divider_component.dart';
--- lib/components/darwin_detail_component.dart
+++ lib/components/darwin_detail_component.dart
@@ -1,5 +1,19 @@
 import 'package:flutter/material.dart';
-import '../../models/models.dart'; // Asegúrate de que la ruta sea correcta
+import '../../models/models.dart';
+import '../../components/components.dart';
+
+Map<String, List<DarwinData>> groupByWeek(List<DarwinData> darwins) {
+  Map<String, List<DarwinData>> map = {};
+  for (var darwin in darwins) {
+    var week = darwin.semana ?? 'Desconocido'; // Proporcionar un valor predeterminado para semanas null
+    if (!map.containsKey(week)) {
+      map[week] = [];
+    }
+    map[week]!.add(darwin);
+  }
+  return map;
+}
+
 
 class DarwinDetailComponent extends StatelessWidget {
   final List<DarwinData> darwins;
@@ -13,37 +27,54 @@ class DarwinDetailComponent extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final groupedDarwins = groupByWeek(darwins);
+
     return Scaffold(
       appBar: AppBar(
         title: Text('$cuc - ${darwins[0].nomcliente}'),
       ),
       body: ListView(
-        children: darwins.map((darwin) => Padding(
-          padding: const EdgeInsets.all(16.0),
-          child: Column(
+        children: groupedDarwins.entries.map((entry) {
+          final week = entry.key;
+          final weekDarwins = entry.value;
+
+          return Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-                Text('Darwin Data ID: ${darwin.darwinDataId}'),
-                Text('Punto de Venta: ${darwin.ptovta}'),
-                Text('Cedis: ${darwin.cedis}'),
-                Text('Marca: ${darwin.marca}'),
-                Text('Código Producto Darwin: ${darwin.codProDarwin}'),
-                Text('Producto: ${darwin.producto}'),
+              Text('Semana: $week'),
+              ...weekDarwins.map((darwin) {
+                return Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                CustomDividerComponent(
+                      text: 'Datos del Preventista',
+                ), 
                 Text('Código Preventa: ${darwin.codPreventa}'),
                 Text('Nombre Preventa: ${darwin.nombrePreventa}'),
+                Text('Cedis: ${darwin.cedis}'),
+                CustomDividerComponent(
+                      text: 'Datos del Producto',
+                ), 
+                Text('Producto: ${darwin.producto}'),
+                Text('Código Producto Darwin: ${darwin.codProDarwin}'),
+                CustomDividerComponent(
+                      text: 'Datos del Dia',
+                ), 
                 Text('Lunes: ${darwin.lun}'),
                 Text('Martes: ${darwin.mar}'),
                 Text('Miércoles: ${darwin.mie}'),
                 Text('Jueves: ${darwin.jue}'),
                 Text('Viernes: ${darwin.vie}'),
                 Text('Sábado: ${darwin.sab}'),
-                Text('Semana: ${darwin.semana}'),
-                Text('Código Empresa: ${darwin.codemp}'),
+                
                 Text('Fecha de Creación: ${darwin.createdOn}'), 
-              SizedBox(height: 16), // Añadir espacio entre los conjuntos de datos
+                  ],
+                );
+              }).toList(),
+              SizedBox(height: 16),
             ],
-          ),
-        )).toList(),
+          );
+        }).toList(),
       ),
     );
   }
