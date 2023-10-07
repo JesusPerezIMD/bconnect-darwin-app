a2c6ee5b2c58015b519a2216d430547dff37df3e
Jesús Alejandro Pérez Ramírez <dual.jesus.perez@gmail.com>
2023-10-06 21:36:52 -0600

    JAPR

--- lib/components/darwin_detail_component.dart
+++ lib/components/darwin_detail_component.dart
@@ -5,7 +5,7 @@ import '../../components/components.dart';
 Map<String, List<DarwinData>> groupByWeek(List<DarwinData> darwins) {
   Map<String, List<DarwinData>> map = {};
   for (var darwin in darwins) {
-    var week = darwin.semana ?? 'Desconocido'; // Proporcionar un valor predeterminado para semanas null
+    var week = darwin.semana ?? 'Desconocido';
     if (!map.containsKey(week)) {
       map[week] = [];
     }
@@ -13,7 +13,6 @@ Map<String, List<DarwinData>> groupByWeek(List<DarwinData> darwins) {
   }
   return map;
 }
-
 
 class DarwinDetailComponent extends StatelessWidget {
   final List<DarwinData> darwins;
@@ -24,6 +23,34 @@ class DarwinDetailComponent extends StatelessWidget {
       required this.darwins, 
       required this.cuc
   }) : super(key: key);
+
+  Widget _buildRow(Icon icon, String title, String value) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 8),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Row(
+            children: [
+              icon, // Display the icon
+              const SizedBox(width: 5),
+              Text(
+                title,
+                style: const TextStyle(fontSize: 12, color: Colors.grey),
+              )
+            ],
+          ),
+          Padding(
+            padding: const EdgeInsets.only(left: 25.0),
+            child: Text(
+              value,
+              style: TextStyle(fontSize: 14, color: Colors.black),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -37,40 +64,46 @@ class DarwinDetailComponent extends StatelessWidget {
         children: groupedDarwins.entries.map((entry) {
           final week = entry.key;
           final weekDarwins = entry.value;
-
           return Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
               Text('Semana: $week'),
-              ...weekDarwins.map((darwin) {
-                return Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
+              CustomDividerComponent(text: 'Datos del Preventista'),
+              _buildRow(const Icon(Icons.info, size: 20), 'Código Preventista:', weekDarwins[0].codPreventa?.toString() ?? ''),
+              _buildRow(const Icon(Icons.info, size: 20), 'Nombre Preventista:', weekDarwins[0].nombrePreventa?.toString() ?? ''),
+              _buildRow(const Icon(Icons.info, size: 20), 'Cedis:', weekDarwins[0].cedis?.toString() ?? ''),
+              CustomDividerComponent(text: 'Datos del Producto'),
+              Padding(
+                padding: const EdgeInsets.only(bottom: 4.0),
+                child: Row(
                   children: [
-                CustomDividerComponent(
-                      text: 'Datos del Preventista',
-                ), 
-                Text('Código Preventa: ${darwin.codPreventa}'),
-                Text('Nombre Preventa: ${darwin.nombrePreventa}'),
-                Text('Cedis: ${darwin.cedis}'),
-                CustomDividerComponent(
-                      text: 'Datos del Producto',
-                ), 
-                Text('Producto: ${darwin.producto}'),
-                Text('Código Producto Darwin: ${darwin.codProDarwin}'),
-                CustomDividerComponent(
-                      text: 'Datos del Dia',
-                ), 
-                Text('Lunes: ${darwin.lun}'),
-                Text('Martes: ${darwin.mar}'),
-                Text('Miércoles: ${darwin.mie}'),
-                Text('Jueves: ${darwin.jue}'),
-                Text('Viernes: ${darwin.vie}'),
-                Text('Sábado: ${darwin.sab}'),
-                
-                Text('Fecha de Creación: ${darwin.createdOn}'), 
+                    Expanded(child: Text('Producto')),
+                    Expanded(child: Text('Codigo')),
+                    Expanded(child: Text('Lun')),
+                    Expanded(child: Text('Mar')),
+                    // ... More headers for other days
                   ],
-                );
-              }).toList(),
+                ),
+              ),
+              ...weekDarwins.map((darwin) => 
+                Row(
+                  children: [
+                    Expanded(
+                      child: Text('${darwin.producto}'),
+                    ),
+                    Expanded(
+                      child: Text('${darwin.codProDarwin}'),
+                    ),
+                    Expanded(
+                      child: Text('${darwin.lun}'),
+                    ),
+                    Expanded(
+                      child: Text('${darwin.mar}'),
+                    ),
+                    //... More days
+                  ],
+                )
+              ).toList(),       
               SizedBox(height: 16),
             ],
           );
