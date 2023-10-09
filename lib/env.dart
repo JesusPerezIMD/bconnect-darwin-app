b42c5a43bc01547b75a5ed5aa6c05957425d6acd
Jesús Alejandro Pérez Ramírez <dual.jesus.perez@gmail.com>
2023-10-09 16:01:38 -0600

    Cambios

--- lib/env.dart
+++ lib/env.dart
@@ -19,9 +19,7 @@ class Environment {
   final UrlApiImages = 'https://bconnect-aliados-api.azurewebsites.net/';
   final TWILIO_API = 'https://twiliofunctionbconnect.azurewebsites.net/api';
   final WHATSAPP_NUMBER = "15165185350";
-  final BITACORA_API =
-      "https://prod-45.westus.logic.azure.com:443/workflows/eb24003a3e604314ab801a7dea5ced7c/triggers/manual/paths/invoke?api-version=2016-06-01&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=pxQuFcqDl6DjWQ-I1m2hzzwi1e1fbt_X5gFNObncOts";
-
+  
   //Production
   // final BCONNECT_API = "https://bconnect-auth-api.azurewebsites.net";
   // final ALIADOS_API = 'https://bconnect-aliados-api-prod.azurewebsites.net/api';
